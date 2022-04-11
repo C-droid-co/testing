aa3ea1c24dbbadcf83de0f3762ea71b947473219
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-11 21:54:04 +0100

    Update sudo.py

--- ftb/modules/sudo.py
+++ ftb/modules/sudo.py
@@ -23,9 +23,9 @@ SOFTWARE.
 """
 from pyrogram import filters
 from pyrogram.types import Message
-from wbb import BOT_ID, SUDOERS, USERBOT_PREFIX, app2, eor
-from wbb.core.decorators.errors import capture_err
-from wbb.utils.dbfunctions import add_sudo, get_sudoers, remove_sudo
+from ftb import BOT_ID, SUDOERS, USERBOT_PREFIX, app2, eor
+from ftb.core.decorators.errors import capture_err
+from ftb.utils.dbfunctions import add_sudo, get_sudoers, remove_sudo
 
 __MODULE__ = "Sudo"
 __HELP__ = """
