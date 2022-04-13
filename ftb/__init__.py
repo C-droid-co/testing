6c24de8af9258dcccd386eb97e8c77e5dbeb14b6
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-13 02:44:25 +0100

    Update __init__.py

--- ftb/__init__.py
+++ ftb/__init__.py
@@ -54,7 +54,7 @@ log.info("Initializing MongoDB client")
 mongo_client = MongoClient(MONGO_URL)
 db = mongo_client.ftb
 if MONGO_URL2 == none
-MONGO_URL2 = MONGO_URL
+    MONGO_URL2 = MONGO_URL
 mongo_client = MongoClient(MONGO_URL2)
 db = mongo_client.ftb2
 
