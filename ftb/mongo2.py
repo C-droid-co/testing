a6ee31a54d990877b377bae56913137a8f1eee55
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-13 03:50:35 +0100

    Update mongo2.py

--- ftb/mongo2.py
+++ ftb/mongo2.py
@@ -2,10 +2,10 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
 from ftb.config import get_str_key
 
-MONGO2 = get_str_key("MONGO_URI_2", None)
-MONGO = get_str_key("MONGO_URI", required=True)
+MONGO2 = get_str_key("MONGO_URL_2", None)
+MONGO = get_str_key("MONGO_URL", required=True)
 if MONGO2 == None:
     MONGO2 = MONGO
 
-mongo_client = MongoClient(MONGO2)
+mongo_client = MongoClient(MONGO_URL_2)
 db = mongo_client.ftb2
