86584434e04e632185320943ca52678e0ad26946
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-13 02:33:48 +0100

    Update __init__.py

--- ftb/__init__.py
+++ ftb/__init__.py
@@ -53,6 +53,7 @@ log = Log(True, "bot.log")
 log.info("Initializing MongoDB client")
 mongo_client = MongoClient(MONGO_URL)
 db = mongo_client.ftb
+MONGO_URL = MONGO_URL2
 
 
 async def load_sudoers():
