cfb67eaf568c913dd41b26e71d8d7fb1fde06d82
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-19 00:07:15 +0100

    Update __init__.py

--- ftb/modules/sql/__init__.py
+++ ftb/modules/sql/__init__.py
@@ -1,11 +1,12 @@
-from ftb import DATABASE_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from ftb import DATABASE_URI as DB_URI
+
 
 def start() -> scoped_session:
-    engine = create_engine(DATABASE_URI, client_encoding="utf8")
+    engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
