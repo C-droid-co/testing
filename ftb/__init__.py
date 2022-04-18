cc8b562803db6ebc701c6b84624f3d8072ea6e10
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-18 19:46:29 +0100

    Update __init__.py

--- ftb/__init__.py
+++ ftb/__init__.py
@@ -139,3 +139,8 @@ async def eor(msg: Message, **kwargs):
     )
     spec = getfullargspec(func.__wrapped__).args
     return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
+telegram.ext.RegexHandler = CustomRegexHandler
+telegram.ext.CommandHandler = CustomCommandHandler
+telegram.ext.MessageHandler = CustomMessageHandler
+
