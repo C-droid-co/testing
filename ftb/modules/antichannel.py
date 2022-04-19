003300b2f791778483dad4945ef445c27b5476d9
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-19 14:02:38 +0100

    Create antichannel.py

---
+++ ftb/modules/antichannel.py
@@ -0,0 +1,7 @@
+__HELP__ = """
+Restrict users from sending as anonymous channels
+ • `/antichannel <on/off/yes/no>`*:* enables antichannel in the current chat
+If enabled, the message from the channel which the user sends will be banned.
+"""
+
+__MODULE__ = "Antichannel"
